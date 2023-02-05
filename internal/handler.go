6c67a53e92a4ba12a36eda6423918a8cb3b0430d
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2023-02-05 12:48:29 +0900

    Use db handler for mocking

--- internal/handler.go
+++ internal/handler.go
@@ -1,33 +1,132 @@
 package internal
 
 import (
-	"errors"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/bwmarrin/snowflake"
+	"github.com/labstack/echo/v4"
+	"gopkg.in/yaml.v3"
 )
 
-func shortenURL(
-	longURL string,
-	shortURLDB DatabaseOperations,
-	longURLDB DatabaseOperations,
-) string {
-	key, exist := shortURLDB.Get(longURL)
+type DBHandler struct {
+	shortURLDB DatabaseOperations
+	longURLDB  DatabaseOperations
+}
+
+var (
+	characterSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	nodeID       = "0" + os.Getenv("NODE_ID")
+	configPath   = os.Getenv("CONFIG")
+	node         *snowflake.Node
+	enc          *Encoding
+	Handler      *DBHandler
+)
+
+type Config struct {
+	RedisServer     string `yaml:"RedisServer"`
+	RedisPW         string `yaml:"RedisPW"`
+	RedisShortURLDB int    `yaml:"RedisShortURLDB"`
+	RedisLongURLDB  int    `yaml:"RedisLongURLDB"`
+}
+
+func init() {
+	var err error
+	var nid int
+
+	// Get the unique node id.
+	if n, err := strconv.Atoi(nodeID); err != nil {
+		nid = rand.Intn(1024)
+	} else {
+		nid = n % 1024
+	}
+
+	// Create a unique id generator.
+	log.Printf("nodeID: %d", nid)
+	node, err = snowflake.NewNode(int64(nid))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Base62 encoder.
+	log.Printf("Base62 characterset: %s", characterSet)
+	enc = NewEncoding(characterSet)
+
+	if len(configPath) == 0 {
+		configPath = "config/default.yaml"
+	}
+
+	// Read the config file.
+	log.Printf("config path: %s", configPath)
+	yfile, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Panic(err)
+	}
+	config := Config{}
+	err = yaml.Unmarshal(yfile, &config)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Init the db client.
+	log.Printf(
+		"Database - server: %s, short url db: %d, long url db: %d",
+		config.RedisServer,
+		config.RedisShortURLDB,
+		config.RedisLongURLDB,
+	)
+
+	// Database handler.
+	Handler = &DBHandler{
+		NewDatabase(config.RedisServer, config.RedisPW, config.RedisShortURLDB),
+		NewDatabase(config.RedisServer, config.RedisPW, config.RedisLongURLDB),
+	}
+}
+
+//	@Summary		Shorten the URL.
+//	@Description	Shorten the URL as 11-length Base62 string.
+//	@Accept			json
+//	@Produce		json
+//	@Param			request	body		ShortenURLReq	true	"url"
+//	@Success		200		{object}	ShortenURLResp
+//	@Failure		400		{object}	error
+//	@Router			/shorten [post].
+func (h *DBHandler) ShortenURL(c echo.Context) error {
+	shortenURLReq := ShortenURLReq{}
+	if err := c.Bind(&shortenURLReq); err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
+	}
+
+	longURL := shortenURLReq.URL
+	key, exist := h.shortURLDB.Get(longURL)
 	if !exist {
 		// Generate a Base62 unique key.
 		key = generateShortURL()
 
 		// Store the value in the database.
-		shortURLDB.Set(longURL, key)
-		longURLDB.Set(key, longURL)
+		h.shortURLDB.Set(longURL, key)
+		h.longURLDB.Set(key, longURL)
 	}
-	return key
+
+	return c.JSON(http.StatusOK, ShortenURLResp{Key: key})
 }
 
-func originalURL(key string, longURLDB DatabaseOperations) (string, error) {
-	longURL, exist := longURLDB.Get(key)
-	var err error
+//	@Summary		Redirect to the original URL.
+//	@Description	Redirect to the original URL.
+//	@Param			key	path		string	true	"key"
+//	@Success		302	{object}	string
+//	@Failure		404	{object}	error
+//	@Router			/{key} [get].
+func (h *DBHandler) OriginalURL(c echo.Context) error {
+	key := c.Param("key")
+	longURL, exist := h.longURLDB.Get(key)
 	if !exist {
-		err = errors.New("key does not exist")
+		return c.String(http.StatusNotFound, key)
 	}
-	return longURL, err
+	return c.Redirect(http.StatusFound, longURL)
 }
 
 // Helpers
